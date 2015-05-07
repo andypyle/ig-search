f4c65cb8c0a8fdfc986474000241ff94e42cc7ac
Andrew Pyle <andy.pyle@gmail.com>
2015-05-07 14:47:07 -0700

    Some more finishing touches. Almost done.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,55 @@
-var app = angular.module('igSearch', []);
+var app = angular.module('igSearch', ['ngAnimate','angular-velocity']);
 
 app.controller('instaController', function($scope, $http, $q, $timeout){
 
-	/*
 	function wait(){
 		return $q(function(resolve, reject){
 			$timeout(function(){
 				resolve();
-			}, 2000);
+			}, 6000);
 		});
-	};
-	*/
+	}
 
 
 
 	$scope.searchGo = function(searchTerm){
-		$scope.searchTermModel = searchTerm;
+		if($scope.searchIg.$valid){
+			$scope.searchTermModel = '';
 
-		var url = 'https://api.instagram.com/v1/tags/' + searchTerm + '/media/recent?callback=JSON_CALLBACK';
-		var req = {
-			client_id		: "bae40bc9101a4dab9b01d676adecc2a1",
-			client_secret 	: "4f7d96476e4046eaa3e135861b5f0c50",
-			count			: 20
-		};
+			$scope.resultModel = searchTerm;
+			$scope.searchStatus = true;
+			$scope.searchDone = false;
 
-		$http({
-			method:'jsonp',
-			url: url,
-			params: req
-		})
-		.success(function(data){
-	      	$scope.results = data;
-	    });
+			$scope.searchIg.$pristine = true;
+
+			var url = 'https://api.instagram.com/v1/tags/' + searchTerm + '/media/recent?callback=JSON_CALLBACK';
+			var req = {
+				client_id		: "bae40bc9101a4dab9b01d676adecc2a1",
+				client_secret 	: "4f7d96476e4046eaa3e135861b5f0c50",
+				count			: 20
+			};
+
+			$http({
+				method:'jsonp',
+				url: url,
+				params: req
+			})
+			.success(function(data){
+
+				$scope.searchStatus = false;
+		      	$scope.results = data;
+		      	$scope.searchDone = true;
+		      	$('.notify > div').velocity('stop');
+		      	
+		    })
+		    .then(wait)
+		    .then(function(){
+		    	$scope.searchDone = false;
+
+
+		    });
+		    return $scope.searchDone;
+		}
 	};
 
 });
