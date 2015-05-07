9b763ead7cb22e945031996c2e1fb8e56d604ba1
Andrew Pyle <andy.pyle@gmail.com>
2015-05-07 14:48:31 -0700

    Some more finishing touches. Almost done.

--- js/app.js
+++ js/app.js
@@ -22,8 +22,9 @@ app.controller('instaController', function($scope, $http, $q, $timeout){
 
 			$scope.searchIg.$pristine = true;
 
-			var url = 'https://api.instagram.com/v1/tags/' + searchTerm + '/media/recent?callback=JSON_CALLBACK';
+			var url = 'https://api.instagram.com/v1/tags/' + searchTerm + '/media/recent';
 			var req = {
+				callback: "JSON_CALLBACK", 
 				client_id		: "bae40bc9101a4dab9b01d676adecc2a1",
 				client_secret 	: "4f7d96476e4046eaa3e135861b5f0c50",
 				count			: 20
@@ -35,18 +36,13 @@ app.controller('instaController', function($scope, $http, $q, $timeout){
 				params: req
 			})
 			.success(function(data){
-
 				$scope.searchStatus = false;
 		      	$scope.results = data;
-		      	$scope.searchDone = true;
-		      	$('.notify > div').velocity('stop');
-		      	
+		      	$scope.searchDone = true;		      	
 		    })
 		    .then(wait)
 		    .then(function(){
 		    	$scope.searchDone = false;
-
-
 		    });
 		    return $scope.searchDone;
 		}
